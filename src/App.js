3c15b70005db8b15b5b78d35ba459d6a48a5651d
Bertill Browne <bertillbrowne@gmail.com>
2020-12-28 11:33:15 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import SignUp from './components/signup/signup.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <SignUp />
+      <h1>Comming Soon: Immersion</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/signup/signup.jsx
@@ -0,0 +1,7 @@
+function SignUp() {
+    return (<h2>Hi I am also a Car!</h2>
+        
+        );
+  }
+
+  export default SignUp;
